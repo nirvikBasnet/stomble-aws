8afcee400c587a1041364dc777990627e34f30da
nirvikBasnet <nirvik41@gmail.com>
2021-09-01 11:45:54 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards';
 import './App.css';
+import 'react-credit-cards/es/styles-compiled.css'
 
 function App() {
+
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [focus, setFocus] = useState(''); //"react-credit-card" needs for styling and animation
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form>
+        <Cards
+          number={number}
+          name={name}
+          expiry={expiry}
+          cvc={cvc}
+          focused={focus}
+        />
+
+        <input
+
+
+          type='tel'
+          name='number'
+          placeholder='Card Number'
+          value={number}
+          maxLength="16"
+          onChange={e => setNumber(e.target.value)}
+          onFocus={e => setFocus(e.target.value)}
+        />
+
+
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={name}
+          onChange={e => setName(e.target.value)}
+          onFocus={e => setFocus(e.target.value)}
+        />
+
+
+        <input
+          type='tel'
+          name='expiry'
+          placeholder='Expiry MM/YY'
+          value={expiry}
+          maxLength="5"
+          onChange={e => setExpiry(e.target.value)}
+          onFocus={e => setFocus(e.target.value)}
+        />
+
+
+        <input
+          type='tel'
+          name='cvc'
+          placeholder='CVC'
+          value={cvc}
+          maxLength="3"
+          onChange={e => setCvc(e.target.value)}
+          onFocus={e => setFocus(e.target.value)}
+        />
+
+
+      </form>
+
     </div>
   );
 }
